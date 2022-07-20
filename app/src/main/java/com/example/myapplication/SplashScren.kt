2dcf834de576d7a3bd8e36588cae923101036a49
Shabri Teli <shabri@softmonks.com>
2022-07-20 23:24:12 +0530

    start = basics

--- app/src/main/java/com/example/myapplication/SplashScren.kt
+++ app/src/main/java/com/example/myapplication/SplashScren.kt
@@ -1,7 +1,15 @@
 package com.example.myapplication
 
+import android.animation.ObjectAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
 import com.example.myapplication.databinding.ActivitySplashScrenBinding
 
 class SplashScren : AppCompatActivity() {
@@ -12,12 +20,28 @@ class SplashScren : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        initialise()
+        splashScreen()
         onClickEvents()
     }
 
-    private fun initialise(){
+    private fun splashScreen(){
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+
+        val titleAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_bounce)
+        binding.callImg.startAnimation(titleAnimation)
     }
 
     private fun onClickEvents(){
